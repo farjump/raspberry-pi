886b442ae1d90caef4cc5601383037f5febd0a1b
Julio Guerra <julio@farjump.io>
2017-02-15 15:40:27 +0100

    sdcard installer
    `scripts/1-install-rpi-boot.sh` is a tool installing everything
    required to run Alpha on any RPi version.
    
    It:
     1. downloads the firmware (bootcode.bin) and bootloader (start.elf)
     2. mounts provided block device into `/tmp`
     3. copies the firmware, bootloader, Alpha.bin and config.txt into the mount-point
     4. checks the integrity using sha256 checksums
     5. un-mounts the block device
    
    You can then plug it into the RPi. It will find everything it needs to run Alpha.

---
+++ scripts/1-install-rpi-boot.sh
@@ -0,0 +1,136 @@
+#!/bin/sh
+
+# immediately exit on any unhandled error
+set -e
+
+# current working directory
+cwd=${0%/*}
+
+. $cwd/lib/sh/utils.sh
+
+. $cwd/config.sh
+
+#
+# Print the help message
+#
+irb_help() {
+  cat <<EOF
+Usage: $0 [--help] <sdcard-block-device>
+
+Download and install into the block device the Raspberry Pi's firmware
+& bootloader, and Farjump's Alpha. The block device must
+contain a FAT file-system.
+
+Note that the block device is mounted and finally unmounted during the
+execution of this scripts, which requires root privileges.
+
+EXAMPLE
+  \$ mkfs.vfat -F 32 -n RPI /dev/sdX # /!\ erases everything in sdX
+  \$ $0 /dev/sdX
+EOF
+}
+
+#
+# Mount the SD card block device `$1` into `$cfg_sdcard_mountpoint`.
+#
+irb_mount() {
+  dev="$1"
+  log_info "Temporarily mounting \`$dev\` into \`$cfg_sdcard_mountpoint\`"
+  mkdir -p $cfg_sdcard_mountpoint
+  sudo mount -t vfat -o rw,umask=0000 "$dev" $cfg_sdcard_mountpoint
+}
+
+#
+# Unmount `$irb_sdcard_mountpoint`
+#
+irb_unmount() {
+  log_info "Un-mounting \`$cfg_sdcard_mountpoint\`"
+  sync $cfg_sdcard_mountpoint/*
+  sudo umount $cfg_sdcard_mountpoint
+}
+
+#
+# Download official firmwares from the RPi repo
+#
+irb_download_files() {
+  dst="$1"
+  if ! irb_check_fw_sha256 $dst --quiet 1>&- 2>&-; then
+      log_info "Downloading the Raspberry Pi's firmware version $cfg_rpi_fw_version"
+      (
+          cd $dst
+          curl -fSL --remote-name-all --progress-bar $cfg_rpi_fw_baseurl/start.elf $cfg_rpi_fw_baseurl/bootcode.bin
+      )
+  fi
+}
+
+#
+# Copy downloaded files into the mounted SD card and check the file integrity.
+#
+irb_copy_files() {
+  src=$1
+  log_info "Installing the RPi firmware and the Alpha debugger"
+  cp -fv $src/bootcode.bin $src/start.elf $src/Alpha.bin $src/config.txt $cfg_sdcard_mountpoint
+  log_info "Checking the integrity"
+  irb_check_fw_sha256 $cfg_sdcard_mountpoint
+  echo "$cfg_alpha_sha256  $cfg_sdcard_mountpoint/Alpha.bin" | sha256sum -c -
+}
+
+#
+# Check the sha256sum of the firmware stored in $1.
+# Optionally pass an extra sha256sum argument as $2.
+#
+irb_check_fw_sha256() {
+  ( echo "$cfg_bootcode_download_sha256  $1/bootcode.bin" | sha256sum -c $2 - ) || return 1
+  echo "$cfg_start_download_sha256  $1/start.elf" | sha256sum -c $2 -
+}
+
+#
+# script entry function
+#
+cbf() {
+  while [ $# -gt 0 ]; do
+    case $1 in
+      -h|--help)
+        irb_help
+        return 1 # note: doing this makes only one --help message possible
+        ;;
+
+      -*)
+        log_error  "$0: illegal option −− $1"
+        return 1
+        ;;
+
+      --)
+        shift
+        ;;
+
+      *)
+        [ -n "$dev" ] && log_info "warning: ignoring \`$dev\`"
+        dev="$1"
+        shift
+        ;;
+    esac
+  done
+
+  if [ -z "$dev" ]; then
+    log_error " missing block device argument"
+    return 1
+  fi
+
+  src='boot'
+  irb_download_files $src
+  irb_mount "$dev"
+  trap 'irb_unmount "$dev"' INT QUIT KILL ABRT
+  if ! irb_copy_files $src; then
+    irb_unmount "$dev"
+    return 1
+  fi
+  irb_unmount "$dev"
+  log_info 'Your SD card is ready!'
+  log_info "You can now insert it into the RPi and use Alpha through the RPI's Mini-UART"
+  return 0
+}
+
+# call the entry function `cbf` if in interactive mode.
+process_is_interactive &&
+  cbf $@
---
+++ scripts/config.sh
@@ -0,0 +1,7 @@
+# configuraion
+cfg_rpi_fw_version='1.20161215'
+cfg_rpi_fw_baseurl="https://github.com/raspberrypi/firmware/raw/$cfg_rpi_fw_version/boot/"
+cfg_sdcard_mountpoint='/tmp/rpi-sdcard-mountpoint'
+cfg_bootcode_download_sha256=db7bd566617565efe3bedfa4d69eaa2ab1d5d73b1de55c0d3bd67e879d2eb7e9
+cfg_start_download_sha256=38a00a91eeafe87ddec042e64025745760dcc77cac2bff0d87a28b11ddce871e
+cfg_alpha_sha256=b1166ac605f33fdf313dbf3d656305248e79130ab4f4a5017c731abefdafecca
