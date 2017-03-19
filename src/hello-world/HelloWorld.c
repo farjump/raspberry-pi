1ee1a3ed6d349193f3f61be2091ad4935b08e1cf
Julio Guerra <julio@farjump.io>
2017-03-19 22:13:39 +0100

    Standard input reading example
    Modify the helloworld example to showcase reading from the
    standard input using `scanf()`, delegated to GDB.
    
    It prompts a message and enables the terminal cursor before
    reading a string. It finally prints the string in the hello
    message.

--- src/hello-world/HelloWorld.c
+++ src/hello-world/HelloWorld.c
@@ -1,6 +1,17 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(void)
 {
-  printf("Hello %s!\n", "RPi");
+  // String prompt and show the terminal cursor
+  printf("Enter a string: \e[?25h");
+  fflush(stdout);
+
+  // Read a string
+  char buffer[20];
+  if (scanf("%19s", &buffer) == 1)
+  {
+    // Say Hi
+    printf("RPi says \"Hello %s!\"\n", buffer);
+  }
 }
